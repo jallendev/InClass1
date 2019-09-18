3a673cfe0f2c45fc5d058c6eb9e68bb89a194c43
Joseph Allen <developer.joseph.allen@gmail.com>
2019-09-18 14:13:15 -0500

    Add an object to FPDriver

--- FPDriver.java
+++ FPDriver.java
@@ -1,10 +1,18 @@
 import java.util.Date;
-
 public class FPDriver {
 
 	public static void main(String[] args) {
 		FloatPoint2 fp = new FloatPoint2(3.2, 2.3);
 		System.out.println(fp);
+		
+		Object o = fp;
+		System.out.println(o);
+		
+		o = new FloatPoint3();
+		System.out.println(o);
+		
+		//FloatPoint3 y = (FloatPoint3)(new Object());
+		//System.out.println(y);
 		
 		Double what = fp.getX();
 		System.out.println(what);

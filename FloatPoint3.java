41aa89b7740caa8514297559366390e0c5fcdd34
Joseph Allen <developer.joseph.allen@gmail.com>
2019-09-18 14:23:39 -0500

    FloatPoint3 code added

--- FloatPoint3.java
+++ FloatPoint3.java
@@ -1,4 +1,16 @@
 
-public class FloatPoint3 {
-
+public class FloatPoint3 extends FloatPoint2 {
+	Double z;
+	
+	public FloatPoint3() {
+		super(10, 11);
+		z = 0.0;
+	}
+	
+	@Override
+	//modify our FloatPoint2 toString
+	public String toString() {
+		String str = super.toString();
+		return str + z.toString();
+	}
 }
